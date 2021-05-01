4d29e9027ff0de6e1ae74aac0614f5b6906c586a
marco <marco.souzadejoode@gmail.com>
2021-05-01 14:40:16 +0200

    Presentation modifications and further testing

--- timing.py
+++ timing.py
@@ -3,19 +3,29 @@ import numpy as np
 from timeit import repeat
 import matplotlib.pyplot as plt
 
+# maximální horní mez pro síta
+MAX = 10**4
 
-Ns = np.linspace(10, 1*10**4, 30, dtype="int64")
+# prostor horních mezí pro hledání prvočísel
+# dolní mez nechme na čísle > 2
+Ns = np.linspace(10, MAX, 30, dtype="int64")
 
 
 
 # nemusíme importovat všechny funkce ručně
 funcs = [func for func in dir(Sieves) if func[0] != "_"]
-funcs.remove("np")
-funcs.remove("deque")
+
+funcs = ["eratosthenes", "eratosthenes_boolean", "eratosthenes_optimized"]
+
+# odstranění nevyhovujícího
+#funcs.remove("np")
+#funcs.remove("deque")
 
 
+# matice výsledných časů
 times_matrix = []
 
+# časování jednotlivých sít
 for func in funcs:
 	times = []
 	for N in Ns:
@@ -24,10 +34,16 @@ for func in funcs:
 		times.append(min(repeat(siv, number=1, repeat=2, globals=globals())))
 	times_matrix.append(np.array(times))
 
+
+# vykreslení grafů
 for i,times in enumerate(times_matrix):
 	plt.scatter(Ns, times*1000, label=f"{funcs[i]}")
 
+
+# popisy os, legenda atp.
 plt.legend(fontsize=20)
 plt.ylabel("T (ms)", fontsize=15)
 plt.xlabel("N", fontsize=15)
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
 plt.show()
